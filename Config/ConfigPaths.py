c6b1f861ca913011d9f06de2ab6d69ab928a41f5
Jan Janiszewski <pole19901990@gmail.com>
2022-12-05 23:11:00 +0100

    cleaned up stuff

---
+++ Config/ConfigPaths.py
@@ -0,0 +1,10 @@
+from Config.Location import location
+
+if location == 'cinthy':
+    training_data= './train_set'
+elif location == 'jesse':
+    pass
+elif location == 'sebas':
+    pass
+elif location == 'jan':
+    training_data = 'Data/Input/food-recognition-challenge/train_set/train_set/train_set'
